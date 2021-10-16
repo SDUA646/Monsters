d2dc6e8bb5258d4f81f1e8b03b36d45972299860
GA-Feng <463004530@qq.com>
2021-10-16 12:20:18 +0800

    可以探视野，不做周围检测

--- Monsters/Form1.Designer.cs
+++ Monsters/Form1.Designer.cs
@@ -37,13 +37,13 @@ namespace Monsters
             // 
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(776, 714);
+            this.groupBox1.Size = new System.Drawing.Size(1160, 714);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(891, 161);
+            this.button1.Location = new System.Drawing.Point(1096, 69);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 1;
@@ -54,7 +54,7 @@ namespace Monsters
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1019, 849);
+            this.ClientSize = new System.Drawing.Size(1249, 849);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
--- Monsters/Objects.cs
+++ Monsters/Objects.cs
@@ -36,6 +36,12 @@ public class Person
     {
 
     }
+
+    public void getView()
+    {
+       
+        
+    }
 }
 
 public class Monster
