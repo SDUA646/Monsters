a4f69077e54e00835103274da784f0e4d2ab5759
GaaarretWang <1509391996@qq.com>
2021-10-25 16:32:48 +0800

    修改图片路径

--- Monsters/Pictures.cs
+++ Monsters/Pictures.cs
@@ -3,38 +3,38 @@
 public class Pictures
 {
 	//定义问号图片路径
-    public string wenhao = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\ask.bmp";
+    public string wenhao = System.Environment.CurrentDirectory + "\\image\\ask.bmp";
     //定义初始后开始按钮的图片
-    public string chushi = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\face.bmp";
+    public string chushi = System.Environment.CurrentDirectory + "\\image\\face.bmp";
     //定义炸死后开始按钮的图片
-    public string siqu = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\sile.jpg";
+    public string siqu = System.Environment.CurrentDirectory + "\\image\\sile.jpg";
     //定义胜利的声音
-    public string shengli = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\烟花.wav";
+    public string shengli = System.Environment.CurrentDirectory + "\\image\\烟花.wav";
     //定义爆炸声音
-    public string shengyin = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\爆炸声.wav";
+    public string shengyin = System.Environment.CurrentDirectory + "\\image\\爆炸声.wav";
     //定义怪1
-    public string monsters = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\怪.png";
+    public string monsters = System.Environment.CurrentDirectory + "\\image\\怪.png";
     //定义右键单击时，标记旗子图片的路径
-    public string hearts = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\hearts.bmp";
+    public string hearts = System.Environment.CurrentDirectory + "\\image\\hearts.bmp";
     //地雷图片位置
-    public string person1 = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\person1.png";
-    public string person2 = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\person2.png";
-    public string person3 = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\person3.png";
-    public string person4 = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\person4.png";
+    public string person1 = System.Environment.CurrentDirectory + "\\image\\person1.png";
+    public string person2 = System.Environment.CurrentDirectory + "\\image\\person2.png";
+    public string person3 = System.Environment.CurrentDirectory + "\\image\\person3.png";
+    public string person4 = System.Environment.CurrentDirectory + "\\image\\person4.png";
     //空的图片
-    public string ground = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\背景.png";
+    public string ground = System.Environment.CurrentDirectory + "\\image\\背景.png";
     //通关图片
-    public string victory = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\face.bmp";
+    public string victory = System.Environment.CurrentDirectory + "\\image\\face.bmp";
     //障碍物
-    public string block = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\block.png";
+    public string block = System.Environment.CurrentDirectory + "\\image\\block.png";
     //障碍物
-    public string rock = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\rock.jpg";
+    public string rock = System.Environment.CurrentDirectory + "\\image\\rock.jpg";
     //加速器
-    public string booster= System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\booster.jpg";
+    public string booster= System.Environment.CurrentDirectory + "\\image\\booster.jpg";
     //钥匙
-    public string key = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\key.jpg";
+    public string key = System.Environment.CurrentDirectory + "\\image\\key.jpg";
     //石头钥匙
-    public string key_1 = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\key.jpg";
+    public string key_1 = System.Environment.CurrentDirectory + "\\image\\key.jpg";
     //绿巨人
-    public string hulk = System.Environment.CurrentDirectory + "\\..\\..\\..\\image\\怪2.png";
+    public string hulk = System.Environment.CurrentDirectory + "\\image\\怪2.png";
 }
